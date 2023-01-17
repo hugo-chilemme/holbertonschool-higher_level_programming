0861d80483dec59efcf7c90480b1de5bbc09ac39
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-01-17 05:36:30 -0800

    8. To uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def uppercase(c):
+    for letter in c:
+        return_ascii = ord(letter)
+        if ord(letter) >= 97 and ord(letter) <= 122:
+            return_ascii -= 32
+        print(chr(return_ascii), end="")
+    print("\n", end="")
