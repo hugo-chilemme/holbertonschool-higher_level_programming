ee6674d06b3fef78ef78c8efdbd4d5d5572b9afa
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-01-17 05:14:31 -0800

    5. 00...99

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -2,7 +2,7 @@
 for number in range(0, 100):
     zero_before = ""
     if number < 10:
-        zero_before = "0"    
+        zero_before = "0"
     if number == 99:
         print("{}".format(number))
     else:
