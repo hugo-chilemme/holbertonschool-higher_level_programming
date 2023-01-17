b1ef9a9697affd51fd169112a123e87f6563425c
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-01-17 05:50:38 -0800

    12. Fizz Buzzé

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for number in range (1, 101):
+    for number in range(1, 101):
         if number % 3 == 0 and number % 5 == 0:
             print("FizzBuzz ", end="")
         elif number % 3 == 0:
