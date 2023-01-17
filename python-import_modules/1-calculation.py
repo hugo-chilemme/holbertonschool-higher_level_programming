b6cd766a10beece5b6baa529bec0405bdd311978
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-01-17 06:23:29 -0800

    1. My first toolbox!

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+from calculator_1 import add
+from calculator_1 import sub
+from calculator_1 import mul
+from calculator_1 import div
+
+a = 10
+b = 5
+
+if __name__ == "__main__":
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, sub(a, b)))
+    print("{} + {} = {}".format(a, b, mul(a, b)))
+    print("{} + {} = {}".format(a, b, div(a, b)))
