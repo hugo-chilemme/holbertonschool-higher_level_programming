bc21deddd74ba7a4d44c6b19bdc86e52b3b749a9
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-01-17 07:34:59 -0800

    4. Who are you?

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,7 @@
 import statistics  
 import hidden_4
 
-for name in dir(hidden_4):
-    if name[:2] != "__":
-        print(name)
+if __name__ == "__main__":
+    for name in dir(hidden_4):
+        if name[:2] != "__":
+            print(name)
