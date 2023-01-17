360c4be281dc2b3673de436385215133548c7357
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-01-17 05:27:16 -0800

    6. Inventing is a combination of brains and materials. The more brains you use, the less material you need

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+separator = ""
+for number in range(0, 100):
+    if number % 11 != 0:
+        last_number = number%10
+        first_number = int(number/10)
+        reverse_number = last_number * 10 + first_number
+        if reverse_number > number:
+            if number < 10:
+                print("{}0{}".format(separator, number), end="")
+            else:
+                print("{}{}".format(separator, number), end="")
+            separator = ", "
+print("\n", end="")
