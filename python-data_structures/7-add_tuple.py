0f9eb6ba9f9cc96350a8c027e10d449461d5d411
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-01-22 15:51:47 -0800

    Task 7: Fix issue: out of range

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -4,12 +4,15 @@ def add_tuple(tuple_a=(), tuple_b=()):
     
     j = 0
     for num in tuple_a:
-        new_tuple[j] += num;
+        if new_tuple[j]:
+            new_tuple[j] += num;
         j += 1
         
     j = 0
     for num in tuple_b:
-        new_tuple[j] += num;
+        if new_tuple[j]:
+            new_tuple[j] += num;
         j += 1
         
     return tuple(new_tuple)
+c
