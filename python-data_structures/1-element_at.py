00c47be05641e666ff68530001d2d0d165b4d905
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-01-22 15:19:41 -0800

    Fix issue: len - 1 size

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0 or len(my_list) < idx:
+    if idx < 0 or len(my_list) - 1 < idx:
         return None
     if my_list[idx]:
         return my_list[idx]
