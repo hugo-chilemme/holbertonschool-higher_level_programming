28ee2c286876aec6639cf728c2d64a8f7e3f8bab
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-01-23 02:04:13 -0800

    End task

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman_manuals = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+    if roman_string is None:
+        return None
+    roman_number = 0
+    for letter in roman_string:
+        if roman_manuals[letter]:
+            roman_number += roman_manuals[letter]
+    return roman_number
