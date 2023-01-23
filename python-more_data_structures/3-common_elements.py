a9e583b432c15b8f699d518213eb8b5785ff9b38
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-01-22 16:53:57 -0800

    Task 3

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -2,7 +2,7 @@
 def common_elements(set_1, set_2):
     callback = []
     for i in set_1:
-         for j in set_2:
-             if j == i:
+        for j in set_2:
+            if j == i:
                 callback.append(i)
     return callback
