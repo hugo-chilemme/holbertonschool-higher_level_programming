9cd3c22003d46981433bb1dca06585aeba6fcff3
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-01-22 16:31:42 -0800

    Task 1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = my_list.copy()
+    for i, val in enumerate(new_list):
+        if val == search:
+            new_list[i] = replace
+    return new_list
