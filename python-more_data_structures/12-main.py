d335ce7ea3198f51387edd0911fad1384477d1d7
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-01-23 02:10:33 -0800

    End task

--- python-more_data_structures/12-main.py
+++ python-more_data_structures/12-main.py
@@ -1,19 +1,6 @@
 #!/usr/bin/python3
-""" Roman to Integer test file
+""" Test file
 """
 roman_to_int = __import__('12-roman_to_int').roman_to_int
 
-roman_number = "X"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "VII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "IX"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "LXXXVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "DCCVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+print(roman_to_int("IV"))
--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -12,7 +12,12 @@ def roman_to_int(roman_string):
     if roman_string is None:
         return None
     roman_number = 0
+    last_number = None
     for letter in roman_string:
         if roman_manuals[letter]:
-            roman_number += roman_manuals[letter]
+            if last_number and last_number < roman_manuals[letter]:
+                roman_number += (roman_manuals[letter] - (last_number + 1))
+            else:
+                roman_number += roman_manuals[letter]
+            last_number = roman_manuals[letter]
     return roman_number
