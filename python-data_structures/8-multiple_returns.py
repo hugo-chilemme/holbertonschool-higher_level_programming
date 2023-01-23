73efa69d7f566ae88704de1fea720991b0693808
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-01-22 16:04:54 -0800

    Task 7 - 11

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     first_letter = None
-    if sentence[0]:
+    if len(sentence) > 0:
         first_letter = sentence[0]
     return len(sentence), first_letter
--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list is None:
+    if my_list is None or len(my_list) == 0:
         return None
-    max = 0;
+    max = -1000
     for num in my_list:
         if num > max:
             max = num
