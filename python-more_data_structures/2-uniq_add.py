cb0a75ce0761487900ea74797a9d82ff27dd9d66
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-01-22 16:37:15 -0800

    Task 2

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+def uniq_add(my_list=[]):
+    new_list = []
+    res = 0
+    for i in my_list:
+        in_array = False
+        for i, val in enumerate(new_list):
+            if val == i:
+                in_array = True
+        if in_array == False:
+            res += i
+            new_list.append(i)
+    return res - 1
+
