2fccd54f914a12f008387989a3cd02ddefee982d
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-01-27 07:54:43 -0800

    Task 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
-    for k, v in enumerate(my_list):
-        if k < x:
-            print("{}".format(v), end="")
-            count += 1
-    print();
-    return (count)        
+    try: 
+        for k, v in enumerate(my_list):
+            if k < x:
+                print("{}".format(v), end="")
+                count += 1
+        print()
+        return (count)
+    except:
+        return (0)
+
