230b9bc9264ee50b5e7ae2f67d6c19c7e516174b
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-01-27 08:13:32 -0800

    Task 3

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def safe_print_division(a, b):
+    result = None
+    try:
+        result = a / b
+    except:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+        return result
