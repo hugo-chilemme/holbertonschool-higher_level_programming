892cd5f72908d77975d014d895beb0b400866d3b
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-01-27 08:10:55 -0800

    Task 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             element = my_list[i]
             try: 
-                print("{:d}".format(my_list[i]), end="")
+                print("{:d}".format(element), end="")
                 count+=1
             except:
                 pass
