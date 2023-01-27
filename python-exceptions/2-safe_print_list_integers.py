1d85f20eec06fa5d554e1db83e316d9be9b69a4d
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-01-27 08:11:10 -0800

    Task 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,9 +4,9 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(0, x):
         try:
             element = my_list[i]
-            try: 
+            try:
                 print("{:d}".format(element), end="")
-                count+=1
+                count += 1
             except:
                 pass
         except (RuntimeError, TypeError, NameError):
