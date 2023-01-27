52ddea1768bff693e9161b5c64067fb43cc9dddf
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-01-27 08:20:49 -0800

    Task 4

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    list = []
+    for i in range(0, list_length):
+        Result = 0
+        try: 
+            element_1 = my_list_1[i]
+            element_2 = my_list_2[i]
+            if element_2 == 0:
+                print("devision by 0")
+                Result = 0
+            else:
+                try: 
+                    Result = element_1 / element_2
+                except:
+                    print("wrong type")
+        except:
+            print("out of range")
+        list.append(Result)
+    return list
