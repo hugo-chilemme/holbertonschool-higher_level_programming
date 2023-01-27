6bd628a66c490dabe48695553c7e40d024b7f6e5
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-01-27 08:08:52 -0800

    Task 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    for k, v in enumerate(my_list):
-        if k < x:
-            try:
-                print("{:d}".format(v), end="")
-                if type(v) is int:
-                    count += 1
-            except:
-                continue
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+        except (RuntimeError, TypeError, NameError):
+            pass
     print()
     return count
