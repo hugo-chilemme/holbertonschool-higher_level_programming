e454f8772692282d4d321f08b6dbf255da9b3c49
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-01-27 07:51:45 -0800

    Task 0

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    for k, v in enumerate(my_list):
+        if k < x:
+            print("{}".format(v), end="")
+            count += 1
+    print();
+    return (count)        
