44d77d55000901aa5f4d8545dbee1f41f0dfa239
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-01-27 08:02:56 -0800

    Task 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for k, v in enumerate(my_list):
+        if k < x:
+            try:
+                print("{:d}".format(v), end="")
+                if type(v) is int:
+                    count += 1
+            except:
+                continue
+    print()
+    return count
