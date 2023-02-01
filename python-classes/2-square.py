75142b2fad13ea6ff813b7ac4b2ba11527932ecf
Hugo Chilemme <73786753+HugoCLI@users.noreply.github.com>
2023-02-01 14:36:17 +0100

    Task 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""Square"""
+
+
+class Square:
+    """Init Square"""
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise ValueError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
