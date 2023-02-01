707ec67afd5c47874c37542bd1854b2af1d666e6
Hugo Chilemme <73786753+HugoCLI@users.noreply.github.com>
2023-02-01 14:40:15 +0100

    Task 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""Square"""
+
+
+class Square:
+    """Init Square"""
+    def __init__(self, size=0):
+       self.size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise ValueError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be an integer")
+        self.__size = value
