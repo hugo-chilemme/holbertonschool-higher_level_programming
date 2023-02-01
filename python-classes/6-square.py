f8c95aab9996afcc37667dd3289181c8111b2d9d
Hugo Chilemme <73786753+HugoCLI@users.noreply.github.com>
2023-02-01 14:48:04 +0100

    Task -

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,1 +1,34 @@
 #!/usr/bin/python3
+"""Square"""
+
+
+class Square:
+    """Init Square"""
+    def __init__(self, size=0, position=(0, 0)):
+        self.__position = position
+        self.size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise ValueError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be an integer")
+        self.__size = value
+
+    def my_print(self):
+        if self.__size == 0:
+            return print()
+        for y in range(0, self.__size):
+            for space in range(0, self.__position[0]):
+                print(" ", end="")
+            for x in range(0, self.__size):
+                print("#", end="")
+            print()
