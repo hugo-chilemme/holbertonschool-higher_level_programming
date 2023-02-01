e725af7e4a7356b654b3774da1a14d85a1f72d34
Hugo Chilemme <73786753+HugoCLI@users.noreply.github.com>
2023-02-01 15:14:20 +0100

    base

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,6 +3,7 @@
 
 
 def add_integer(a, b=98):
+    """Return a + b"""
     if isinstance(a, (int, float)) is False:
         raise TypeError("a must be an integer")
     if isinstance(b, (int, float)) is False:
