5514138dfc0704fd04cf851f07d2108883878e03
Hugo Chilemme <73786753+HugoCLI@users.noreply.github.com>
2023-02-01 14:56:00 +0100

    Task 6

--- python-classes/6-main.py
+++ python-classes/6-main.py
@@ -2,6 +2,6 @@
 Square = __import__('6-square').Square
 
 try:
-    my_square = Square(3, "Position")
+    my_square = Square(3, (1, ))
 except Exception as e:
     print(e)
--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -29,7 +29,9 @@ class Square:
     
     @position.setter
     def position(self, value):
-        if type(value) is not tuple or value[0] < 0 or value[1] < 0:
+        if type(value) is not tuple or len(value) < 2:
+            raise ValueError("position must be a tuple of 2 positive integers")
+        if type(value[0]) is not int or type(value[1]) is not int or value[0] < 0 or value[1] < 0:
             raise ValueError("position must be a tuple of 2 positive integers")
         self.__position = value
 
