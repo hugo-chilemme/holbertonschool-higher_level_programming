833c6143d55b5f47c6bf3ac3bad48824b03ffc73
Hugo Chilemme <73786753+HugoCLI@users.noreply.github.com>
2023-02-03 09:22:25 -0800

    Task 2

--- python-test_driven_development/3-main.py
+++ python-test_driven_development/3-main.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
-print_square = __import__('4-print_square').print_square
+say_my_name = __import__('3-say_my_name').say_my_name
 
-print_square(4)
-print("")
-print_square(10)
-print("")
-print_square(0)
-print("")
-print_square(1)
-print("")
+say_my_name("John", "Smith")
+say_my_name("Walter", "White")
+say_my_name("Bob")
 try:
-    print_square(-1)
+    say_my_name(12, "White")
 except Exception as e:
     print(e)
-print("")
--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,1 +1,11 @@
+#!/usr/bin/python3
+"""say_my_name"""
 
+
+def say_my_name(first_name="", last_name=""):
+    """function that prints My name is <first name> <last name"""
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
