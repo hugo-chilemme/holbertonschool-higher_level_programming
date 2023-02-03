2a15524cb53170aab749dd6a604a1c8452028019
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-02-03 03:34:28 -0800

    Task 1

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -8,7 +8,7 @@ def matrix_divided(matrix=[], div=1):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-
+    error = "matrix must be a matrix (list of lists) of integers/floats"
     size = None
     new_matrix = []
     j = 0
@@ -19,8 +19,7 @@ def matrix_divided(matrix=[], div=1):
         size = len(i)
         for element in i:
             if not isinstance(element, (int, float)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError(error)
             new_matrix[j].append(round(element / div, 2))
-        j += 1 
+        j += 1
     return new_matrix
-    
