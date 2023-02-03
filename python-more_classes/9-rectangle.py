884eee58df6aba53a234e54bdcac830eb2884862
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-02-03 02:37:33 -0800

    Fix issue in Rectangle.__init__ order of width and height

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -9,8 +9,8 @@ class Rectangle:
     print_symbol = "#"
 
     def __init__(self, width=0, height=0):
-        self.height = height
         self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     @property
