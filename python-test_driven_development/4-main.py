bfda3344b225fd841050f7ee9027b415905b442d
Hugo Chilemme <73786753+HugoCLI@users.noreply.github.com>
2023-02-03 11:41:10 -0800

    Task 4

--- python-test_driven_development/4-main.py
+++ python-test_driven_development/4-main.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-text_indentation = __import__('5-text_indentation').text_indentation
+print_square = __import__('4-print_square').print_square
 
-text_indentation("""Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
-Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere? \
-Non autem hoc: igitur ne illud quidem. Fortasse id optimum, sed ubi illud: \
-Plus semper voluptatis? Teneo, inquit, finem illi videri nihil dolere. \
-Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum \
-rationi oboediens. Si id dicis, vicimus. Inde sermone vario sex illa a Dipylo \
-stadia confecimus. Sin aliud quid voles, postea. Quae animi affectio suum \
-cuique tribuens atque hanc, quam dico. Utinam quidem dicerent alium alio \
-beatiorem! Iam ruinas videres""")
+print_square(4)
+print("")
+print_square(10)
+print("")
+print_square(0)
+print("")
+print_square(1)
+print("")
+try:
+    print_square(-1)
+except Exception as e:
+    print(e)
+print("")
