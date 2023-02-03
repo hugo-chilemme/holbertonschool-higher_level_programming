31812482ff8a55cb6254173109f50b173575cc06
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-02-03 01:39:27 -0800

    Fix issue in Rectangle.__init__ : call set method

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -5,8 +5,8 @@
 class Rectangle:
     """Rectangle"""
     def __init__(self, width = 0, height = 0):
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
     
     @property
     def width(self):
@@ -22,7 +22,7 @@ class Rectangle:
         
     @property
     def height(self):
-        return self.__width
+        return self.__height
         
     @height.setter
     def height(self, value):
