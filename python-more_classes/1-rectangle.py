841c71e2b1bf461ae03cf64ddbe2647901d76c7e
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-02-03 01:40:40 -0800

    Fix issue in Rectangle : PEP8 correction

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -4,10 +4,10 @@
 
 class Rectangle:
     """Rectangle"""
-    def __init__(self, width = 0, height = 0):
+    def __init__(self, width=0, height=0):
         self.height = height
         self.width = width
-    
+
     @property
     def width(self):
         return self.__width
@@ -19,11 +19,11 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-        
+
     @property
     def height(self):
         return self.__height
-        
+
     @height.setter
     def height(self, value):
         if type(value) is not int:
