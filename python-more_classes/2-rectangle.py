92f97a5a682d18a32016cd98598424db5c434607
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-02-03 01:47:10 -0800

    Fix issue in Rectangle : PEP8 correction

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -34,7 +34,7 @@ class Rectangle:
 
     def area(self):
         return self.height * self.width
-    
+
     def perimeter(self):
         if self.width == 0 or self.height == 0:
             return 0
