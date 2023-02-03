4a73405bf275afce24eb21f75c1ebf4ab4a03e6d
Hugo Chilemme <73786753+HugoCLI@users.noreply.github.com>
2023-02-03 09:24:39 -0800

    Task 2

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -3,7 +3,7 @@
 
 
 def say_my_name(first_name="", last_name=""):
-    """function that prints My name is <first name> <last name"""
+    """function that prints My name is <first name> <last name>"""
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
