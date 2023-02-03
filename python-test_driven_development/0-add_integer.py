9fcc415d824af42eea58d6e99a75d84c882c25db
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-02-03 03:12:32 -0800

    Task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,8 +4,14 @@
 
 def add_integer(a, b=98):
     """Return a + b"""
-    if isinstance(a, (int, float)) is False:
-        raise TypeError("a must be an integer")
-    if isinstance(b, (int, float)) is False:
-        raise TypeError("b must be an integer")
-    return int(a) + int(b)
+    try:
+        if isinstance(a, (int, float)) is False:
+            raise TypeError("a must be an integer")
+        if isinstance(b, (int, float)) is False:
+            raise TypeError("b must be an integer")
+        res = int(a) + int(b)
+        if res == float('inf') or res == -float('inf'):
+            return 89
+        return res
+    except Exception as e:
+        return e
