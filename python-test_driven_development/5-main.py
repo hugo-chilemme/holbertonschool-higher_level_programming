187495f642655539769f7ba14c512b567fb39ba3
Hugo Chilemme <73786753+HugoCLI@users.noreply.github.com>
2023-02-03 11:51:34 -0800

    Creating function text_indentation

--- python-test_driven_development/5-main.py
+++ python-test_driven_development/5-main.py
@@ -1,18 +1,12 @@
 #!/usr/bin/python3
-"""Module to find the max integer in a list
-"""
+text_indentation = __import__('5-text_indentation').text_indentation
 
-
-def max_integer(list=[]):
-    """Function to find and return the max integer in a list of integers
-        If the list is empty, the function returns None
-    """
-    if len(list) == 0:
-        return None
-    result = list[0]
-    i = 1
-    while i < len(list):
-        if list[i] > result:
-            result = list[i]
-        i += 1
-    return result
+text_indentation("""Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere? \
+Non autem hoc: igitur ne illud quidem. Fortasse id optimum, sed ubi illud: \
+Plus semper voluptatis? Teneo, inquit, finem illi videri nihil dolere. \
+Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum \
+rationi oboediens. Si id dicis, vicimus. Inde sermone vario sex illa a Dipylo \
+stadia confecimus. Sin aliud quid voles, postea. Quae animi affectio suum \
+cuique tribuens atque hanc, quam dico. Utinam quidem dicerent alium alio \
+beatiorem! Iam ruinas videres""")
--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,16 @@
+#!/usr/bin/python3
+"""print_square"""
 
+
+def text_indentation(text):
+    """Function that prints a text with 2 new lines after each of these characters: ., ? and :"""
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    
+    for k, v in enumerate(text):
+        if k > 0:
+            before = text[k - 1]
+            if before in [".", "?", ":"]:
+                print("\n")
+                continue
+        print(v, end="")
