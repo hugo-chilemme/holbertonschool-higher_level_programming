1d43b05409b4e5c202339589bca5114880e0cb6e
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-02-07 08:07:19 -0800

    Fix issue in 0-read_file: back line error

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,7 +5,6 @@ def read_file(filename=""):
     """function that reads a text file (UTF8) and prints it to stdout:"""
     
     with open(filename) as f:
-        lines = f.readlines()
-        for line in lines:
-            print(line, end="")
+        lines = f.read()
+        print(lines)
     print("")
