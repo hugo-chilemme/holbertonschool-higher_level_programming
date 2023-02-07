2e1bbcf523bd17105b706a4c38f77c05a2d6d863
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-02-07 08:37:06 -0800

    Task 2

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -4,14 +4,13 @@
 
 def append_write(filename="", text=""):
     """that appends a string at the end of a text file (UTF8)"""
-    try:
-        with open(filename, "r") as r:
-            r.write(text)
-            length = len(r.read())
-            r.close()
-            return length
-    except:
-        f = open(filename, "w")
-        f.write(text)
-        f.close()
-        return len(text)
+    with open(filename, "a") as myfile:
+        myfile.write(text)
+        length = len(text)
+        try:
+            length += myfile.readlines()
+        except:
+            pass
+        myfile.close()
+        return length
+    return 0
--- python-input_output/2-main.py
+++ python-input_output/2-main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 append_write = __import__('2-append_write').append_write
 
-nb_characters_added = append_write("file_append.txt", "This School is so cool!\n")
+filename = "file_2"
+text = "Holberton School is so cool!\n"
+nb_characters_added = append_write(filename, text)
 print(nb_characters_added)
