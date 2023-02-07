cdd885ad4031d909eb3dd4114da86d7dfb1b304c
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-02-07 08:18:04 -0800

    Task 2

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""append_write"""
+
+
+def append_write(filename="", text=""):
+    """that appends a string at the end of a text file (UTF8)"""
+    f = open(filename, "r")
+    f.write(text)
+    f.close()
+    with open(filename) as f:
+        return len(f.read())
