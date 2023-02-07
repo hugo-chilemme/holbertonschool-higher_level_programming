1cc9d8477cfb837314d3cc255ef86b337ab4c492
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-02-07 08:09:55 -0800

    Fix issue in 0-read_file: PEP8 correction

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """read_file"""
 
+
 def read_file(filename=""):
     """function that reads a text file (UTF8) and prints it to stdout:"""
-    
     with open(filename) as f:
         lines = f.read()
         print(lines, end="")
