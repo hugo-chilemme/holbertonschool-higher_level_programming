8f9e72fc9d9c2274e07eb73eb1905b3e0f85ecd3
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-02-07 08:41:46 -0800

    Fix issue: length of only output

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -4,9 +4,9 @@
 
 def append_write(filename="", text=""):
     """that appends a string at the end of a text file (UTF8)"""
+    length = 0
     with open(filename, "a") as myfile:
-        myfile.write(text)
-        length = len(text)
+        length = myfile.write(text)
         myfile.close()
         return length
     return 0
