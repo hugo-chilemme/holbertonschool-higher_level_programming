f44275fe8830fb00e024b53d10b2f98ad654534c
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-02-09 23:54:08 -0800

    Adding function to convert json to object

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,1 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/python3*
+"""import json"""
+
+import json
+"""from_json_string"""
+
+
+def from_json_string(my_obj):
+    """that returns the JSON representation of an object (string)"""
+    return json.loads(my_obj)
