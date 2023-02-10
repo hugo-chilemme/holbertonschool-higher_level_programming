2294aac90d2fafe1459bb8033729cec022f035f1
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-02-10 00:02:14 -0800

    Adding function tu get a json in a file

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -7,5 +7,5 @@ import json
 
 def load_from_json_file(filename):
     """Write a function that creates an Object from a json file"""
-    with open(filename, "w") as f:
+    with open(filename) as f:
         return json.loads(f.readlines())
