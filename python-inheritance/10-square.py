2d70a279561801cecbae62890cda325bc09cb293
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-02-10 09:23:22 -0800

    Task 10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -13,8 +13,6 @@ class Square(Rectangle):
 
     def __str__(self):
         return "[Rectangle] {}/{}".format(self.__size, self.__size)
-    
+
     def area(self):
         return self.__size * self.__size
-
-
