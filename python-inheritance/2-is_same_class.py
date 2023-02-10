1d00781bef67ba900f1670ad0c5eb1951ccdc2e1
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-02-10 08:17:45 -0800

    task 1

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""MyList"""
+"""is_same_class"""
 
 def is_same_class(obj, a_class):
+    """if the object is exactly an instance of the specified class"""
     if isinstance(obj, int) and issubclass(a_class, int):
         return True
     return False
