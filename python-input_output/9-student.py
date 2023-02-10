383e56c181d8a9eb1923f45618d9283060337cca
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-02-10 01:24:54 -0800

    Create class Student

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -8,7 +8,7 @@ class Student:
         self.__first_name = first_name
         self.__last_name = last_name
         self.__age = age
-    
+
     def to_json(self):
         return {
             "first_name": self.__first_name,
