cdebfc5b0a497444f3b6c1a62cf564c5859d23a2
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-02-09 23:57:24 -0800

    Adding function to save json in a file

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -7,5 +7,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """that writes an Object to a text file, using a JSON representation"""
-    with open(filename) as f:
+    with open(filename, "w") as f:
         f.write(json.dumps(my_obj))
