aa61ff582eff2c3013100d0122f24e84ad960dfb
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-02-10 02:08:36 -0800

    Adding reload_from_json()

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -5,41 +5,16 @@
 class Student:
 
     def __init__(self, first_name, last_name, age):
-        self.__first_name = first_name
-        self.__last_name = last_name
-        self.__age = age
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self, attrs=None):
-        keys = ["first_name", "last_name", "age"]
         json = {}
-        for element in keys:
-            if attrs is None or element in attrs:
-                json[element] = vars(self)["_Student__"+element]
+        for k, v in enumerate(self.__dict__):
+            if attrs is None or i in attrs:
+                json[v] = self.__dict__[v]
         return json
-
-    @property
-    def first_name(self):
-        return self.__first_name
-
-    @first_name.setter
-    def first_name(self, value):
-        self.__first_name = value
-
-    @property
-    def last_name(self):
-        return self.__last_name
-
-    @last_name.setter
-    def last_name(self, value):
-        self.__last_name = value
-        
-    @property
-    def age(self):
-        return self.__last_name
-
-    @age.setter
-    def age(self, value):
-        self.__age = value
 
     def reload_from_json(self, json):
         if "first_name" in json:
