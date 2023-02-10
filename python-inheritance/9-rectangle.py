dde9316796fddea10edf711409224044897256be
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-02-10 09:13:57 -0800

    Task 9

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""import"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""Rectangle"""
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle"""
+    def __init__(self, width, height):
+        BaseGeometry.integer_validator(self, "width", width)
+        BaseGeometry.integer_validator(self, "height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        print("[Rectangle] {}/{}".format(self.__width, self.__height))
+        return self.__width * self.__height
