7f136202a9847169a568b092a728fb6f30889e73
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-02-10 01:07:37 -0800

    Get name and number in class

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -4,15 +4,4 @@
 
 def class_to_json(obj):
     """that returns the dictionary description with simple data structure"""
-    json = {}
-    if obj is None:
-        return json
-    try:
-        json["name"] = obj.name
-    except:
-        pass
-    try:
-        json["number"] = obj.number
-    except:
-        pass
-    return json
+    return vars(obj)
