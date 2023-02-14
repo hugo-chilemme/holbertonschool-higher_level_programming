9255aa42f022d57c147afbfebdd8b55b55908265
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-02-14 09:18:01 -0800

    Adding function getter/setter in Square named size

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -11,6 +11,7 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
+        self.__size = size
 
     def __str__(self):
         return "[Square] ({}) {}/{} - {}".format(
@@ -19,3 +20,13 @@ class Square(Rectangle):
             self.y,
             self.width
         )
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+        self.__size = value
