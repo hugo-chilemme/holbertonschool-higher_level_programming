5d953504587cf150eb31ae5e5319e56fd799df34
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-02-14 01:08:50 -0800

    Task 1

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -8,8 +8,24 @@ from models.base import Base
 class Rectangle(Base):
     """Class Rectangle"""
     def __init__(self, width, height, x=0, y=0, id=None):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
         self.__x = x
         self.__y = y
         super().__init__(id)
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__width
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
