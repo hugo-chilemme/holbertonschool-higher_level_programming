188c1b5421d7911cb77747e3a78ade8b790f702f
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-02-14 01:05:43 -0800

    Task 1

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -5,6 +5,7 @@
 class Base:
     """Class Base"""
     __nb_objects = 0
+
     def __init__(self, id=None):
         if id is None:
             Base.__nb_objects += 1
--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+from models.base import Base
+"""Class Rectangle"""
+
+
+class Rectangle(Base):
+    """Class Rectangle"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        super().__init__(id)
