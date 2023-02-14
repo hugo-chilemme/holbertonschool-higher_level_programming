51c70cc43e397102e9ff9003ef75918d9061f291
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-02-14 01:19:34 -0800

    Updating functions in rectangle: Adding in y, x, width, height function setter field verification

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -18,30 +18,46 @@ class Rectangle(Base):
     def width(self):
         return self.__width
 
-    @width.setter
-    def width(self, value):
-        self.__width = value
-
     @property
     def height(self):
         return self.__height
-
-    @height.setter
-    def height(self, value):
-        self.__height = value
-
+    
     @property
     def x(self):
         return self.__x
-
-    @x.setter
-    def x(self, value):
-        self.__x = value
-
+    
     @property
     def y(self):
         return self.__y
 
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @x.setter
+    def x(self, value):
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
     @y.setter
     def y(self, value):
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
