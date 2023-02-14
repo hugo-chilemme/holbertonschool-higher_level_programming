26f7ea6b31b41ffa8358d83d756d599ca3765fbf
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-02-14 09:34:26 -0800

    Adding function in base to convert object to json

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,6 +2,10 @@
 """Class Base"""
 
 
+from json import dumps
+"""Import dumps to convert object to json"""
+
+
 class Base:
     """Class Base"""
     __nb_objects = 0
@@ -12,3 +16,8 @@ class Base:
             self.id = Base.__nb_objects
         else:
             self.id = id
+
+    def to_json_string(list_dictionaries):
+        if not list_dictionaries or len(list_dictionaries) == 0 or type(list_dictionaries) is not list:
+            return "[]"
+        return dumps(list_dictionaries)
