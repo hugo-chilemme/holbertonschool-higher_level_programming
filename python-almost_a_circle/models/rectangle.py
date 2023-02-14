50bee6fdb5d84039e387b2d82391a9464c89a540
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-02-14 05:02:07 -0800

    Adding function in Rectangle: area

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -6,7 +6,7 @@ from models.base import Base
 
 
 class Rectangle(Base):
-    """Class Rectangle"""
+    """Class rectangle inherit from Base"""
     def __init__(self, width, height, x=0, y=0, id=None):
         self.width = width
         self.height = height
