f77f2c5ce1f76ca1b725672d2ec9eef409d2c91e
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-02-14 06:56:23 -0800

    Adding class square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""Class Square"""
+
+
+from models.rectangle import Rectangle
+"""Class Base"""
+
+
+class Square(Rectangle):
+    """Class Square"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id,
+            self.x,
+            self.y,
+            self.width
+        )
