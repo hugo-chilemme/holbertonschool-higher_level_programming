1e989cd403964b2733e0792cb5a13b6cd0b5adfc
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-02-14 05:37:12 -0800

    Fix issue in Rectangle.__str__: PEP8 correction

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -8,7 +8,6 @@ from models.base import Base
 
 class Rectangle(Base):
     """Class rectangle inherit from Base"""
-    
     def __init__(self, width, height, x=0, y=0, id=None):
         self.width = width
         self.height = height
