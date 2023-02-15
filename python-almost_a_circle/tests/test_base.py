abfb565fc9322c896e21a1ffd3b848cbe104a013
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-02-15 09:48:31 -0800

    Units test

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -24,15 +24,34 @@ class TestBase(unittest.TestCase):
         self.assertEqual(b1.id, 1.1)
         
     def test_5(self):
-        b1 = Base.to_json_string([])
+        Base.to_json_string([])
 
     def test_6(self):
-        b1 = Base.save_to_file([])
+        Base.save_to_file([])
 
     def test_7(self):
-        b1 = Base.save_to_file(None)
+        Base.save_to_file(None)
 
+    def test_8(self):
+        Base.to_json_string([])
 
+    def test_9(self):
+        Base.to_json_string([ { 'id': 12 }])
+
+    def test_10(self):
+        Base.from_json_string(None)
+
+    def test_11(self):
+        Base.from_json_string("[]")
+
+    def test_12(self):
+        Base.from_json_string("[]")
+
+    def test_13(self):
+        Base.from_json_string('[{ "id": 89 }]')
+
+    def test_14(self):
+        Base.from_json_string('[{ "id": 89 }]')
 
 if __name__ == '__main__':
     unittest.main()
