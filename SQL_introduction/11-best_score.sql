2583b14463d3154b162802343f27c7073e36ea4a
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-03-15 09:29:57 +0000

    Show all rows with >= 10

---
+++ SQL_introduction/11-best_score.sql
@@ -0,0 +1,2 @@
+-- Write a script that lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0 in your MySQL server.
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC
