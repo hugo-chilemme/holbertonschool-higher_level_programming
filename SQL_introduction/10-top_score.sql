e02b041c3b3d375a9a76b45a82f6b0195b87f899
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-03-15 09:29:08 +0000

    Write a script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
 -- Write a script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
-SELECT * FROM second_table ORDER BY score DESC
+SELECT score, name FROM second_table ORDER BY score DESC
