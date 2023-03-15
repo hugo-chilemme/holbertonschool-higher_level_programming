1f61ff0ff6b8216ea5bae3e92e22c2de1b5cfc13
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-03-15 09:31:22 +0000

    Update rows with value bob

---
+++ SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,2 @@
+-- Write a script that lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0 in your MySQL server.
+UPDATE second_table SET score = 10 WHERE name = "Bob";
