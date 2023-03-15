b8fe9a0b222a8d027c3718cf39f60dbb9f05dd01
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-03-15 09:38:07 +0000

    display order

---
+++ SQL_introduction/16-no_link.sql
@@ -0,0 +1,2 @@
+-- Write a script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
+SELECT score, name FROM second_table ORDER BY score DESC, name ASC
