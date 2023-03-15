f1c33ca60887d099f48d1e7902a49706979f958a
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-03-15 09:37:03 +0000

    count score

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- Write a script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in your MySQL server.
-SELECT score, count(score) as number FROM second_table ORDER BY score DESC GROUP BY score 
+SELECT score, count(score) as number FROM second_table GROUP BY score ORDER BY score DESC
