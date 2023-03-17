a2af47e848ffe02d0263b5f78fca0879623ccf82
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-03-17 14:05:11 +0000

    Display title and genre_id is null

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,6 @@
--- Write a script that lists all shows contained in the database hbtn_0d_tvshows.
+-- Write a script that lists all shows contained in hbtn_0d_tvshows without a genre linked.
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_show_genres.genre_id = NULL
-ORDER BY tv_shows.title, tv_show_genres.genre_id;
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.id IS NULL OR tv_show_genres.show_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
