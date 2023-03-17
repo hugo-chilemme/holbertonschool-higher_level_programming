7af999bf7260882522d36aaefd2409bc90135c96
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-03-17 13:59:36 +0000

    Display tv

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- Write a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
 SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
     FROM tv_show_genres
-    INNER JOIN tv_shows ON tv_show_genres.genre_id = tv_shows.id
+    INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
     ORDER BY tv_shows.title, tv_show_genres.genre_id
 
