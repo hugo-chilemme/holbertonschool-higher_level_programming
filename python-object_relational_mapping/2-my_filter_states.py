c7c0035a71af16f5bf19795764db593eb6b8fe06
root <root@vps-55a540cf.vps.ovh.net>
2023-03-17 14:55:05 +0000

    task 3

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -16,7 +16,35 @@ if __name__ == "__main__":
     )
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC")
+    #!/usr/bin/python3
+"""Lists all states with a name starting with N (upper N)"""
+
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host='localhost',
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
+
+    cur = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name \
+                    LIKE BINARY '{}' ORDER BY states.id ASC"
+                   .format(sys.argv[4])
+                   )
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
+
 
     rows = cur.fetchall()
     for row in rows:
