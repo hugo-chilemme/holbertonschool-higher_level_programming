32c16f636dd11f39a32902365e4fc27fc86e3ba9
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-03-17 15:04:20 +0000

    task 3

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -16,8 +16,8 @@ if __name__ == "__main__":
     )
 
     cur = db.cursor()
-    cur.execute("""SELECT * FROM states WHERE name LIKE = %s"""
-                (sys.argv[4]))
+    cur.execute("""SELECT * FROM states WHERE name \
+                = % s ORDER BY states.id ASC""", (sys.argv[4],))
 
     rows = cur.fetchall()
     for row in rows:
