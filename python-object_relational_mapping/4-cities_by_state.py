a99511dd4e6bc75e040bb03cf341e321b391dbf6
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-03-17 15:03:24 +0000

    task 3

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Lists all states with a name starting with N (upper N)"""
+
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host='localhost',
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
+
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states WHERE name LIKE = %s"""
+                (sys.argv[4]))
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
