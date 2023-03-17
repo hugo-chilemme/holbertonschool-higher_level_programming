4132bb791a56dbce7a8b8142ff1ba4b5cde0ad60
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-03-17 14:25:06 +0000

    Count all show in every genre

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- Write a script that lists all shows contained in hbtn_0d_tvshows without a genre linked.
 SELECT tv_shows.title, tv_genres.name
-    FROM tv_show_genres
-    LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-    LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+    FROM tv_shows
+    LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+    LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
     ORDER BY tv_shows.title ASC, tv_genres.name ASC;
