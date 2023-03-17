f11de0a70ece3b2e5ebb18adf0505b35c8977136
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-03-17 14:09:44 +0000

    Count all show in every genre

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,5 +2,5 @@
 SELECT tv_genres.name, COUNT(tv_show_genres.show_id) AS number_of_shows
     FROM tv_genres 
     INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.show_id
-    GROUP BY number_of_shows
+    GROUP BY tv_genres.name
     ORDER BY number_of_shows DESC
