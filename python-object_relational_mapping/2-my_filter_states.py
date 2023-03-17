e19a0a788631a5c991e57a16c187c204d285b3d2
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-03-17 14:56:57 +0000

    task 3

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -36,16 +36,8 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name \
                     LIKE BINARY '{}' ORDER BY states.id ASC"
-                   .format(sys.argv[4])
-                   )
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-
-    cur.close()
-    db.close()
-
-
+                   .format(sys.argv[4]))
+    
     rows = cur.fetchall()
     for row in rows:
         print(row)
