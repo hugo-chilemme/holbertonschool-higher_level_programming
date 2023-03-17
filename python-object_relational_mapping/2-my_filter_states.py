700302f26dd2a035a99b3a36001877844301bd2b
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-03-17 14:57:33 +0000

    task 3

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -16,24 +16,6 @@ if __name__ == "__main__":
     )
 
     cur = db.cursor()
-    #!/usr/bin/python3
-"""Lists all states with a name starting with N (upper N)"""
-
-
-import MySQLdb
-import sys
-
-
-if __name__ == "__main__":
-    db = MySQLdb.connect(
-        host='localhost',
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        port=3306
-    )
-
-    cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name \
                     LIKE BINARY '{}' ORDER BY states.id ASC"
                    .format(sys.argv[4]))
