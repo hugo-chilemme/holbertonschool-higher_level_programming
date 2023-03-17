fd0f90c0060a68cd49397785aef03f73843a1a74
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-03-17 14:03:24 +0000

    Display title and genre_id is null

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -3,4 +3,4 @@ SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
-WHERE tv_show_genres.genre_id IS NULL
+WHERE tv_show_genres.genre_id = NULL
