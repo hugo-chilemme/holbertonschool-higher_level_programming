3edc790540604a703f911bea586735c855960aa2
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-03-17 21:17:59 +0000

    Task 11

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Lists all states with a name starting with N (upper N)"""
+
+
+import MySQLdb
+import sys
+from model_state import Base, State
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+        .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+
+    session.commit()
+    session.close()
