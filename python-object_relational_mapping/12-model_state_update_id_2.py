e4307c929e38dbf9bffcbcfe7888d45e8cacb4c4
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-03-17 21:25:14 +0000

    Task 12

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Lists all states with a name starting with N (upper N)"""
+
+
+import MySQLdb
+import sys
+from model_state import Base, State
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+        .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    
+    state = session.query(State).filter(State.id == 2).first()
+    state.name = "New Mexico"
+    session.add(state)
+
+    session.commit()
+    session.close()
