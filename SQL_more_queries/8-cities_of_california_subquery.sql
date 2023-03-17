6478aea18672adad1e7e82823ded05370f3df082
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-03-17 13:51:09 +0000

    Select all cities in California

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- Write a script that lists all the cities of California that can be found in the database hbtn_0d_usa.
-SELECT cities.id as id, cities.name FROM states 
+SELECT cities.id AS id, cities.name AS name FROM states 
     INNER JOIN cities ON cities.state_id = states.id 
     WHERE states.name = "California"
