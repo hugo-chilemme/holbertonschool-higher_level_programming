c3df253f6e456791a2e33f552dc8c4501ad0fd79
root <root@vps-55a540cf.vps.ovh.net>
2023-03-17 14:48:11 +0000

    task 1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Write a script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa:"""
+"""Lists all states with a name starting with N (upper N)"""
 
 
 import MySQLdb
