4385d1fb79c9c7656fac41aaacb12a23ecc1e43c
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-03-17 21:00:04 +0000

    Task 5

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Lists all states with a name starting with N (upper N)"""
+
+
+import MySQLdb
+import sys
+from model_state import Base, State
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+        .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).first()
+
+    if state:
+        print(f'{state.id}: {state.name}')
+    else:
+        print('Nothing')
+
+    session.commit()
+    session.close()
