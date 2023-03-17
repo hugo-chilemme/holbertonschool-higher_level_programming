0717a6f3d88c04a611a104ba96f4a0b901070482
Hugo Chilemme <73786753+HugoCLI@users.noreply.github.com>
2023-03-17 14:11:10 +0100

    Task 0

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,3 @@
+-- Write a script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
