620e5744d60c4a3506ab119206ebbf2653b9218b
root <root@vps-55a540cf.vps.ovh.net>
2023-03-17 14:37:45 +0000

    task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -8,11 +8,11 @@ import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
-        host = 'localhost',
-        user = sys.argv[1],
-        passwd = sys.argv[2],
-        db = sys.argv[3],
-        port = 3306
+        host='localhost',
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
     )
 
     cur = db.cursor()
