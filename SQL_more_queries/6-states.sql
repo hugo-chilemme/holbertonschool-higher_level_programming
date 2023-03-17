ff5557e268824a2ee41ed5dbf74b700971023502
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-03-17 13:43:22 +0000

    Create db and table state

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,6 @@
 -- Write a script that creates the table force_name on your MySQL server.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-use hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states(
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
     id int NOT NULL AUTO_INCREMENT,
     name varchar(256),
 );
