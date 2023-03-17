581b7ae04dba8030f8b9835743651a20046f55f9
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-03-17 13:27:52 +0000

    Create user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- Write a script that creates the MySQL server user user_0d_1.
-CREATE LOGIN user_0d_1 WITH PASSWORD = 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS user_0d_2@localhost;
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
