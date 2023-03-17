10ea91b04a00ba7d82101fb317c4b0e6391351d0
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-03-17 19:07:46 +0000

    Task 5

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -9,6 +9,7 @@ Base = declarative_base()
     
     
 class State(Base):
+    """ Class of state"""
     __tablename__ = 'states'
     
     id = Column(Integer, primary_key=True, autoincrement=True)
