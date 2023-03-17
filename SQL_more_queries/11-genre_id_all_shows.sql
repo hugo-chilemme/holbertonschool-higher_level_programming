059a05fa0721f30b8e58b14e5a2a8a19654a1651
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-03-17 14:01:15 +0000

    Display title and genre

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,5 @@
+-- Write a script that lists all shows contained in the database hbtn_0d_tvshows.
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
