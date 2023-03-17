dd40aea8db4743d34a8379627180bb3eb2dbc66d
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-03-17 13:35:46 +0000

    Read user

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- Write a script that creates the database hbtn_0d_2 and the user user_0d_2.
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
-ALTER USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS user_0d_2@localhost;
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
+ALTER USER user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
