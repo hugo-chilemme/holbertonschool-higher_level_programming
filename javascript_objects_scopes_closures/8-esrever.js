eff68893c7e557b24723f6ac3b72a1d3d51e5196
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-04-09 20:43:00 +0000

    Create function to reverse list

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,2 +1,22 @@
 #!/usr/bin/node
 'use strict';
+
+
+/**
+ * esrever
+ * @param {array} list - List of elements
+ * @exemple esrever([1, 2, 3]) => [3, 2, 1]
+ */
+exports.esrever = list => {
+    let new_list = [];
+
+    for ( let i = list.length - 1; i >= 0; i--)
+    {
+
+        const element = list[i];
+        new_list.push(element)
+
+    }
+
+    return new_list
+}
