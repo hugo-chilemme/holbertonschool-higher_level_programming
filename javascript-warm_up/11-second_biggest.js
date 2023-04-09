c5d36c721872a1494e48c744ed3402eae1b2d1ad
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-04-09 17:58:19 +0000

    Create beautiful function to return the max number

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+'use strict';
+
+/**
+ * Create function to return the max number
+ */
+const args = process.argv.slice(2);
+
+const maximum = (numbers = []) => {
+  if (numbers.length === 0)
+    return 0
+  return Math.max(...numbers)
+};
+
+console.log(maximum(args));
