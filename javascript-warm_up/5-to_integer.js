172a889bcbef9187c6a06c76771fe54583a62614
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-04-09 14:38:22 +0000

    adding the sentence before the number 'My number is'

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -2,4 +2,8 @@
 'use strict';
 const args = process.argv.slice(2);
 
-if (isNaN(parseInt(args[0]))) { console.log('Not a number'); } else { console.log(parseInt(args[0])); }
+if (isNaN(parseInt(args[0]))) { 
+    console.log('Not a number'); 
+} else { 
+    console.log(`My number is ${parseInt(args[0])}`);
+}
