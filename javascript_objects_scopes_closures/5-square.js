682683f79c303d189ca40107feaab711af5bd199
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-04-09 20:23:24 +0000

    Creating Class Square extends Rectangle

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,2 +1,36 @@
 #!/usr/bin/node
 'use strict';
+
+
+const Rectangle = require('./4-rectangle');
+
+/**
+ * class Square
+ */
+class Square extends Rectangle {
+    
+
+    /**
+     * constructor
+     * @param {number} size - Size
+     * new Square(2);
+     */
+    constructor(size) {
+
+        super();
+        // When rectangle values are valid
+        if ( size > 0 )
+        {
+
+            this.width = size;
+            this.height = size;
+            
+        }
+
+
+    }
+
+
+}
+
+module.exports = Square
