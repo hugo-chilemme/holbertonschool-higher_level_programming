0b7029445281424e5318d6c3a2b48c0ddb355fda
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-04-09 16:45:45 +0000

    This semistandard is not funny

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -7,17 +7,14 @@
 const args = process.argv.slice(2);
 const SIZE = parseInt(args[0]);
 
-if ( SIZE && !isNaN(SIZE)) {
-    
-    // Creating a line with @SIZE 'X'
-    const line = 'X'.repeat(SIZE);
+if (SIZE && !isNaN(SIZE)) {
+  // Creating a line with @SIZE 'X'
+  const line = 'X'.repeat(SIZE);
 
-    // Creating a line with so many Xs
-    for ( let nline = 0 ; nline < SIZE ; nline++ )
-        console.log(line);
-
-}
-else
-{
-    console.log('Missing size');
+  // Creating a line with so many Xs
+  for (let nline = 0; nline < SIZE; nline++) {
+    console.log(line);
+  }
+} else {
+  console.log('Missing size');
 }
