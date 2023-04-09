01b0eec6f0f119350f0a6d72cf765f053f1effa8
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-04-09 20:45:52 +0000

    Create function logme

--- javascript_objects_scopes_closures/9-logme.js
+++ javascript_objects_scopes_closures/9-logme.js
@@ -1,2 +1,16 @@
 #!/usr/bin/node
 'use strict';
+
+
+/**
+ * esrever
+ * @param {string} item - Message
+ * @exemple esrever([1, 2, 3]) => [3, 2, 1]
+ */
+let logMeList = [];
+exports.logMe  = item => {
+    
+    console.log(`${logMeList.length}: ${item}`);
+    logMeList.push(item);
+
+}
