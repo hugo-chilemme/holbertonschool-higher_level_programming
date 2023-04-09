ffa05894fe42a083d417d8a342b233136dde66d2
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-04-09 20:52:44 +0000

    Create function to convert another base

--- javascript_objects_scopes_closures/10-converter.js
+++ javascript_objects_scopes_closures/10-converter.js
@@ -1,2 +1,20 @@
 #!/usr/bin/node
 'use strict';
+
+
+/**
+ * converter
+ * @param {number} base - New base number
+ * @exemple converter(2)
+ */
+exports.converter = base => {
+    
+    const convert_base = number => {
+
+        return parseInt(number, base);
+
+    }
+    
+    return convert_base;
+
+}
--- javascript_objects_scopes_closures/9-logme.js
+++ javascript_objects_scopes_closures/9-logme.js
@@ -8,7 +8,7 @@
  * @exemple esrever([1, 2, 3]) => [3, 2, 1]
  */
 let logMeList = [];
-exports.logMe  = item => {
+exports.logMe = item => {
     
     console.log(`${logMeList.length}: ${item}`);
     logMeList.push(item);
