50f8ce6294a13ac207af49a82819a81917def8a7
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-04-09 20:39:21 +0000

    Create script to check occurence of a element in a list

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -1,2 +1,26 @@
 #!/usr/bin/node
 'use strict';
+
+/**
+ * nbOccurences
+ * @param {array} list - List of elements
+ * @param {char} searchElement = Element to search
+ * nbOccurences([1, 2, 3], 3)
+ */
+exports.nbOccurences = (list, searchElement) => {
+    let occurrences = 0;
+
+    for ( const element of list )
+    {
+
+        if ( element == searchElement )
+        {
+
+            occurrences += 1;
+
+        }
+
+    }
+
+    return occurrences
+}
