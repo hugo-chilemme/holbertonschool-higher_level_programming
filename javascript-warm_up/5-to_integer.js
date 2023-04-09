9fb1caf4b0927f746d43a609df166f94711c84eb
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-04-09 14:39:47 +0000

    Fix issue semistandard validation

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -2,8 +2,8 @@
 'use strict';
 const args = process.argv.slice(2);
 
-if (isNaN(parseInt(args[0]))) { 
-    console.log('Not a number'); 
-} else { 
+if(isNaN(parseInt(args[0]))) {
+    console.log('Not a number');
+} else {
     console.log(`My number: ${parseInt(args[0])}`);
 }
