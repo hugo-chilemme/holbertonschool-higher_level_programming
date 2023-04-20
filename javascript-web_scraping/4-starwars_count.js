0298286a39f5707106d548bfcf1d59987f2f6cbf
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-04-20 12:01:39 +0000

    Changing script to get id

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -3,15 +3,16 @@
 
 const request = require('request');
 
-const findUrl = 'https://swapi-api.hbtn.io/api/people/18/';
-
 request(process.argv[2], (error, response, body) => {
   if (!error && response.statusCode === 200) {
     const films = JSON.parse(body).results;
     let count = 0;
     for (const film of films) {
-      if (film.characters.includes(findUrl)) {
-        ++count;
+      for (const author of film.characters) {
+        const id = author.split('/people/')[1].split('/')[0];
+        if (parseInt(id) === 18) {
+          ++count;
+        }
       }
     }
     console.log(count);
