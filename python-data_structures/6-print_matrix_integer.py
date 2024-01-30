21c87a2504c821c2aceb3f84375bdd3ea09c64c2
Hugo Chilemme <hugo.chilemme@gmail.com>
2024-01-30 10:15:48 +0100

    Trying code with map method

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
+
 def print_matrix_integer(matrix=[[]]):
-    for x in matrix:
-        i = 0
-        for y in x:
-            if i != len(x) - 1:
-                print("{:d}".format(y), end=" ")
-            else:
-                print("{:d}".format(y), end="")
-            i += 1
-        print("")
+   for row in matrix:
+        print("{}".format(' '.join(map(str, row))))
