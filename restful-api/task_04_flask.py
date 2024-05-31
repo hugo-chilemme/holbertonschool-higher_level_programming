10fa6b981536d1a36880b169311066b8c0c61554
hugo-chilemme <hugo.chilemme@gmail.com>
2024-05-31 16:08:06 +0200

    Create API users

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -40,7 +40,7 @@ def add_user():
 
 	if data is None or data.get('username') is None:
 
-		return jsonify({'error': 'Missing username parameters'})
+		return jsonify({'error': 'Missing username parameters'}), 400
 
 	user = {
 		'username': data.get('username'),
