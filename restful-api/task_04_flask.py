855b10e439a259ebd8a2d46439231d2ea2ade7fe
hugo-chilemme <hugo.chilemme@gmail.com>
2024-05-31 16:04:59 +0200

    Create API users

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,58 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+users = {}
+
+@app.route('/')
+def home():
+	return 'Welcome to the Flask API!'
+
+
+@app.route('/status')
+def status():
+	return 'OK'
+
+
+@app.route('/data')
+def data():
+	return jsonify(list(users)), 200
+
+
+@app.route('/users/<username>')
+def users_username(username):
+
+	if username is None:
+
+		return jsonify({'error': 'Username is required'}), 400
+	
+	if username not in users:
+
+		return jsonify({'error': 'User not found'}), 404
+	
+	return users.get(username)
+
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+
+	data = request.json
+
+	if data.get('username') is None:
+
+		return jsonify({'error': 'Missing username parameters'})
+
+	user = {
+		'username': data.get('username'),
+		'name': data.get('name'),
+		'age': data.get('age'),
+		'city': data.get('city')
+	}
+
+	users[user.get('username')] = user
+
+	return jsonify({'message': 'User added', 'user': user}), 201
+
+
+if __name__ == "__main__":
+	app.run()
