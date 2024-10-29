4ac2c4ec7d7da51a1b70bc154f7537166c0b968a
Hugo Chilemme <hugo.chilemme@gmail.com>
2024-10-29 09:06:51 +0000

    Fix negative radius

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -16,6 +16,10 @@ class Shape(ABC):
 class Circle(Shape):
 
     def __init__(self, radius):
+
+        if radius <= 0:
+            raise ValueError("Perimeter should handle negative radius")
+
         self.__radius = radius
 
     def area(self):
